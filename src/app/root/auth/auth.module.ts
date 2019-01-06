f1ada162761d3e77f82480996765c81814c74fff
Filip Ditrich <ditrich@visionarygraphics.cz>
2019-01-06 04:17:34 +0100

    Auth module fix

--- src/app/root/auth/auth.module.ts
+++ src/app/root/auth/auth.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NbAuthModule } from '@nebular/auth';
 import { NbSpinnerModule } from '@nebular/theme';
 import { HttpHeadersInterceptor } from '../../@core/services/http–interceptor.service';
 import { SharedModule } from '../../@shared/shared.module';
@@ -18,6 +19,7 @@ import { AuthRouting } from './auth.routing';
     ThemeModule,
     NbSpinnerModule,
     SharedModule,
+    NbAuthModule,
   ],
   declarations: [
     AuthComponent,
